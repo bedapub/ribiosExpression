fb5390a7bffa53afada13745fcb444ae99dc74b5
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-08 09:15:52 +0200

    use collate directive to specify that AllGeneric.R should come first

--- R/DesignContrast.R
+++ R/DesignContrast.R
@@ -300,35 +300,14 @@ parseDesignContrast <- function(designFile=NULL, contrastFile=NULL,
     return(res)
 }
 
-#' Return indices of samples involved in the given contrast of two or more coefficients
-#'
-#' @param object: A \code{DesignContrast} object
-#' @param contrast: Either a contrast name or a integer indicating the index of the contrast
-#' @return An integer vector, indices of samples that are involved, sorted by the ascending order of the coefficients of the contrast
-#' 
-#' @examples
-#' ## one-way ANOVA
-#' myDesCon <- parseDesignContrast(sampleGroups="As,Be,As,Be,As,Be",
-#'    groupLevels="Be,As", dispLevels="Beryllium,Arsenic", contrasts="As-Be")
-#' contrastSampleIndices(myDesCon, 1L)
-#' myInterDesCon <- new("DesignContrast", 
-#'     design=matrix(c(rep(1,6), rep(0,2), rep(1,2), rep(0,2), 
-#'            rep(0,4), rep(1,2)), nrow=6, byrow=FALSE), 
-#'     contrasts=matrix(c(0,1,0, 0,0,1, 0,-1,1), byrow=FALSE, nrow=3), 
-#'     groups=factor(rep(c("As", "Be", "Cd"), each=2)), 
-#'     dispLevels=c("Arsenic", "Beryllium", "Cadmium"))
-#' cont1Ind <- contrastSampleIndices(myInterDesCon, 1L)
-#' cont2Ind <- contrastSampleIndices(myInterDesCon, 2L)
-#' cont3Ind <- contrastSampleIndices(myInterDesCon, 3L)
-#' stopifnot(identical(cont1Ind, 1:4))
-#' stopifnot(identical(cont2Ind, c(1:2, 5:6)))
-#' stopifnot(identical(cont3Ind, c(3:6)))
+#' @include AllGenerics.R
+#' @describeIn contrastSampleIndices Use character string to specify the contrast
 #' @export
 setMethod("contrastSampleIndices", c("DesignContrast", "character"), function(object, contrast) {
               .contrastSampleIndices(object, contrast)
           })
 
-#' @describeIn contrastSampleIndices,DesignContrast,character-method The method to use integer indexes to specify the contrast
+#' @describeIn contrastSampleIndices Use integer indices to specify the contrast
 #' @export
 setMethod("contrastSampleIndices", c("DesignContrast", "numeric"), function(object, contrast) {
               .contrastSampleIndices(object, contrast)
