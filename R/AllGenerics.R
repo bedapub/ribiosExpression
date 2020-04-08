c993381e2ec47f1dfe24edc8e4fd0dd8f924b916
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-08 09:15:04 +0200

    it's possible to ducmnet the S4 generic with examples, and use @aliases there and @describeIn in methods to make sure that people understand

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -15,5 +15,32 @@ setGeneric("designMatrix", function(object) standardGeneric("designMatrix"))
 setGeneric("contrastMatrix", function(object) standardGeneric("contrastMatrix"))
 #' @exportMethod nContrast
 setGeneric("nContrast", function(object) standardGeneric("nContrast"))
+
+
+#' Return indices of samples involved in the given contrast of two or more coefficients
+#'
+#' @title 
+#' @param object: A \code{DesignContrast} object
+#' @param contrast: Either a contrast name or a integer indicating the index of the contrast
+#' @return An integer vector, indices of samples that are involved, sorted by the ascending order of the coefficients of the contrast
+#' 
+#' @aliases contrastSampleIndices,DesignContrast,numeric-method contrastSampleIndices,DesignContrast,character-method
+#' @examples
+#' ## one-way ANOVA
+#' myDesCon <- parseDesignContrast(sampleGroups="As,Be,As,Be,As,Be",
+#'    groupLevels="Be,As", dispLevels="Beryllium,Arsenic", contrasts="As-Be")
+#' contrastSampleIndices(myDesCon, 1L)
+#' myInterDesCon <- new("DesignContrast", 
+#'     design=matrix(c(rep(1,6), rep(0,2), rep(1,2), rep(0,2), 
+#'            rep(0,4), rep(1,2)), nrow=6, byrow=FALSE), 
+#'     contrasts=matrix(c(0,1,0, 0,0,1, 0,-1,1), byrow=FALSE, nrow=3), 
+#'     groups=factor(rep(c("As", "Be", "Cd"), each=2)), 
+#'     dispLevels=c("Arsenic", "Beryllium", "Cadmium"))
+#' cont1Ind <- contrastSampleIndices(myInterDesCon, 1L)
+#' cont2Ind <- contrastSampleIndices(myInterDesCon, 2L)
+#' cont3Ind <- contrastSampleIndices(myInterDesCon, 3L)
+#' stopifnot(identical(cont1Ind, 1:4))
+#' stopifnot(identical(cont2Ind, c(1:2, 5:6)))
+#' stopifnot(identical(cont3Ind, c(3:6)))
 #' @exportMethod contrastSampleIndices
 setGeneric("contrastSampleIndices", function(object, contrast) standardGeneric("contrastSampleIndices"))
