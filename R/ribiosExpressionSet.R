6f3978767b41a6b1ac68e33a09ba01c095d2a726
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-08-31 10:31:22 +0200

    document ribiosExpressionSet

---
+++ R/ribiosExpressionSet.R
@@ -0,0 +1,7 @@
+#' ribiosExpressionSet: An example of ExpressionSet with artificial expression data
+#'
+#' @name ribiosExpressionSet
+#' @docType data
+#' @author Jitao David Zhang \email{jitao_david.zhang@roche.com}
+#' @keywords data
+NULL
