dd5f029555d00a9da8613905d38dbed9d00e23aa
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-09-16 17:00:29 +0200

    with function fixDesignMatrixColnames added

---
+++ R/fixDesignMatrixColnames.R
@@ -0,0 +1,37 @@
+#' Fix design matrix colnames so that they are legal variable names
+#' @param designMatrix A design matrix, produced by \code{model.matrix}
+#' @param interceptChar Character string, the value the interaction symbol (:) should be replaced with
+#' @param removeContrastNames Logical, whether the contrast variable name should be removed.
+#' 
+#' @return The matrix with fixed colum names.
+#' @importFrom stats model.matrix
+#' @examples 
+#' myFac1 <- gl(6,2, labels=sprintf("Fac1_%d", 1:6))
+#' myFac2 <- gl(2,6, labels=c("Ctrl", "Dis"))
+#' myVar <- rnorm(12)
+#' myDesign <- model.matrix(~myFac1 * myFac2 + myVar)
+#' head(myDesign)
+#' head(fixDesignMatrixColnames(myDesign))
+#' @export
+fixDesignMatrixColnames <- function(designMatrix,
+                            interceptChar="_",
+                            removeContrastNames=TRUE) {
+  contrasts <- attr(designMatrix, "contrasts")
+  stopifnot(length(removeContrastNames)==1 |
+              length(removeContrastNames)==length(contrasts))
+  removeContrastNames <- rep_len(removeContrastNames, 
+                                 length.out=length(contrasts))
+  cnames <- colnames(designMatrix)
+  varnames <- names(contrasts)
+  for (i in seq(along=varnames)) {
+    if(removeContrastNames[i]) {
+      vn <- varnames[i]
+      cnames <- sub(vn, "", cnames)
+    }
+  }
+  cnames <- gsub(":", interceptChar, cnames)
+  cnames[1] <- "Baseline"
+  cnames <- make.names(cnames)
+  colnames(designMatrix) <- cnames
+  return(designMatrix)
+}
---
+++ tests/testthat/test-fixDesignMatrixColnames.R
@@ -0,0 +1,48 @@
+library(ribiosExpression)
+library(testthat)
+
+context("Test fixDesignMatrixColnames")
+
+myFac1 <- gl(6,2, labels=sprintf("Fac1_%d", 1:6))
+myFac2 <- gl(2,6, labels=c("Ctrl", "Dis"))
+myVar <- rnorm(12)
+myDesignAdd <- model.matrix(~myFac1 + myFac2 + myVar)
+myDesignAddFixed <- fixDesignMatrixColnames(myDesignAdd)
+myDesignInt <- model.matrix(~myFac1 * myFac2 + myVar)
+myDesignIntFixed <- fixDesignMatrixColnames(myDesignInt)
+
+myDesignIntFixedPoint <- fixDesignMatrixColnames(myDesignInt,
+                                                 interceptChar = ".")
+myDesignAddFixedKeepContrastName <- fixDesignMatrixColnames(myDesignAdd,
+                                                            removeContrastNames = FALSE)
+
+expectedAddColnames <- c("Baseline", "Fac1_2", "Fac1_3", "Fac1_4", 
+                         "Fac1_5", "Fac1_6", "Dis", "myVar")
+expectedIntColnames <- c("Baseline", "Fac1_2", "Fac1_3", "Fac1_4", 
+                         "Fac1_5", "Fac1_6", "Dis", "myVar",
+                         "Fac1_2_Dis", "Fac1_3_Dis", "Fac1_4_Dis", "Fac1_5_Dis", "Fac1_6_Dis")
+
+test_that("fixDesignMatrixColnames works for additive models", {
+  expect_equivalent(myDesignAdd, myDesignAddFixed)
+  expect_identical(colnames(myDesignAddFixed),
+                   expectedAddColnames)
+})
+
+test_that("fixDesignMatrixColnames works for additive models, without removing contrast names", {
+  expect_equivalent(myDesignAdd, myDesignAddFixedKeepContrastName)
+  expect_identical(colnames(myDesignAddFixedKeepContrastName),
+                   gsub("Dis", "myFac2Dis", gsub("Fac1", "myFac1Fac1", expectedAddColnames)))
+})
+
+
+test_that("fixDesignMatrixColnames works for interaction models", {
+  expect_equivalent(myDesignInt, myDesignIntFixed)
+  expect_setequal(colnames(myDesignIntFixed),
+                  expectedIntColnames)
+})
+
+test_that("fixDesignMatrixColnames works with interceptChar='.'", {
+  expect_equivalent(myDesignInt, myDesignIntFixedPoint)
+  expect_setequal(colnames(myDesignIntFixedPoint),
+                  gsub("_Dis", ".Dis", expectedIntColnames))
+})
