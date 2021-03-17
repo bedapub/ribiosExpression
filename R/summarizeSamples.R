09defc2083c67f55e9b873ae8259bd1d27c631dc
Jitao David Zhang <jitao_david.zhang@roche.com>
2021-03-17 13:55:48 +0100

    summarizeSamples supports all objects in assayData; add medianSamples

--- R/summarizeSamples.R
+++ R/summarizeSamples.R
@@ -7,8 +7,11 @@
 #' \code{poolReplicates} and \code{avgReplicates} are two specific form of the
 #' more generic \code{summarizeSamples} function: they take sum and average of
 #' replicates given by the factor, respectively.
-#' 
-#' @aliases summarizeSamples poolReplicates avgReplicates
+#'
+#' From version 1.1-7, the function summarizes not only \code{exprs}, but also
+#' all other objects in \code{assayData}
+#'
+#' @aliases summarizeSamples poolReplicates avgReplicates medianReplicates
 #' @param eset An \code{eSet} object.
 #' @param indSamples A factor of the same length as the sample number of the
 #' object
@@ -32,7 +35,7 @@
 #' 
 #' (ss.eset2 <- summarizeSamples(ribios.ExpressionSet, index, fun=mean,
 #' na.rm=TRUE))
-#' ## equivalently 
+#' ## equivalently
 #' (ss.eset2 <- poolReplicates(ribios.ExpressionSet, index))
 #' 
 #' (ss.eset3 <- avgReplicates(ribios.ExpressionSet, index))
@@ -42,14 +45,21 @@ summarizeSamples <- function(eset, indSamples=eset$SAMPLEID, removeInvarCols=TRU
   if(!is.factor(indSamples)) indSamples <- as.factor(indSamples)
   stopifnot(length(indSamples)==ncol(eset))
   if(is.factor(indSamples))  indSamples <- droplevels(indSamples)
-  eset.pool <- ribiosUtils::summarizeColumns(exprs(eset), indSamples, fun=fun, ...)
+  assayDataEnv <- new.env()
+  assayDataItems <- ls(assayData(eset))
+  for(item in assayDataItems) {
+    item.pool <-  ribiosUtils::summarizeColumns(get(item,
+                                                    envir=assayData(eset)),
+                                                indSamples, fun=fun, ...)
+    assign(item, item.pool, envir=assayDataEnv)
+  }
   eset.pd <- do.call(rbind, tapply(1:ncol(eset), indSamples, function(x) {
     pData(eset)[x[1],]
   }))
   if(removeInvarCols) eset.pd <- removeInvarCol(eset.pd)
-  rownames(eset.pd) <- colnames(eset.pool)
+  rownames(eset.pd) <- colnames(get("exprs", envir=assayDataEnv))
   eset.poolEset <- new("ExpressionSet",
-                       exprs=eset.pool,
+                       assayData=assayDataEnv,
                        phenoData=new("AnnotatedDataFrame", eset.pd),
                        featureData=featureData(eset))
   return(eset.poolEset)
@@ -66,4 +76,11 @@ poolReplicates <- function(eset, indSamples=eset$SAMPLEID, removeInvarCols=TRUE)
 #' @export
 avgReplicates <- function(eset, indSamples=eset$SAMPLEID, removeInvarCols=TRUE) {
   summarizeSamples(eset, indSamples=indSamples, removeInvarCols=TRUE, fun=mean, na.rm=TRUE)
+}
+
+#' @rdname medianSamples
+#' @export
+medianReplicates <- function(eset, indSamples=eset$SAMPLEID, removeInvarCols=TRUE) {
+  summarizeSamples(eset, indSamples=indSamples,
+        removeInvarCols=TRUE, fun=median, na.rm=TRUE)
 }
